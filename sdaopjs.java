b6131a8bd0af856161ad62db1b27a9b2db4a482b
iraivg <iraivg@users.noreply.github.com>
2020-11-27 13:51:13 +0200

    uhu

--- sdaopjs.java
+++ sdaopjs.java
@@ -12,6 +12,7 @@ public class sdaopjs {
 		for(int i=0; i<n; i++) {
 			System.out.println("Enter the element of the array: ");  
 			arr[i]=input.nextInt();
+                        SFGB
 		
 		}
 		int max = arr[0];
